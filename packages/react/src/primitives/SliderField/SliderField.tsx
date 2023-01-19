3c6effb00ab176809d9efedc67aa418e02e843c9
Chenwei Zhang <chenwz@amazon.com>
2023-01-19 11:49:22 -0800

    chore: tag release to reproduce radix bug

--- packages/react/src/primitives/SliderField/SliderField.tsx
+++ packages/react/src/primitives/SliderField/SliderField.tsx
@@ -1,5 +1,5 @@
 import classNames from 'classnames';
-import * as RadixSlider from '@radix-ui/react-slider';
+import { Range, Root, Thumb, Track } from '@radix-ui/react-slider';
 import * as React from 'react';
 
 import { classNameModifier } from '../shared/utils';
@@ -17,10 +17,9 @@ import { useStableId } from '../utils/useStableId';
 
 // Radix packages don't support ESM in Node, in some scenarios(e.g. SSR),
 // the module will be imported as CommonJS module, in which we have to reference the `default`
-let sanitizedRadixSlider = { default: undefined, ...RadixSlider };
+// let sanitizedRadixSlider = { default: undefined, ...RadixSlider };
 // eslint-disable-next-line @typescript-eslint/no-unsafe-assignment
-sanitizedRadixSlider = sanitizedRadixSlider.default ?? sanitizedRadixSlider;
-const { Range, Root, Thumb, Track } = sanitizedRadixSlider;
+// sanitizedRadixSlider = sanitizedRadixSlider.default ?? sanitizedRadixSlider;`
 
 export const SLIDER_LABEL_TEST_ID = 'slider-label';
 export const SLIDER_ROOT_TEST_ID = 'slider-root';
