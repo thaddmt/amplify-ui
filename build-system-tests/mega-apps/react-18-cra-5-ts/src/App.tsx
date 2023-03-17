45818e18b092c4469a8b4ee1b85d2d359e1c7a88
MJ☔️ <o.6180339+github@gmail.com>
2023-03-17 00:39:45 +0000

    test(build system):react-18-cra-5-ts add amplify

--- build-system-tests/mega-apps/react-18-cra-5-ts/src/App.tsx
+++ build-system-tests/mega-apps/react-18-cra-5-ts/src/App.tsx
@@ -1,7 +1,40 @@
-import React from 'react';
+import { useEffect } from 'react';
+import { Amplify, Notifications } from 'aws-amplify';
+import {
+  Authenticator,
+  FileUploader,
+  MapView,
+  Text,
+  InAppMessagingProvider,
+  InAppMessageDisplay,
+} from '@aws-amplify/ui-react';
+import '@aws-amplify/ui-react/styles.css';
+import awsconfig from './aws-exports';
+Amplify.configure(awsconfig);
 
-function App() {
-  return <div className="App">test</div>;
+const { InAppMessaging } = Notifications;
+
+export default function Home() {
+  useEffect(() => {
+    // sync remote in-app messages
+    InAppMessaging.syncMessages();
+  }, []);
+  return (
+    <>
+      <InAppMessagingProvider>
+        <InAppMessageDisplay />
+        <Text>In-App Messaging Example</Text>
+      </InAppMessagingProvider>
+      <FileUploader acceptedFileTypes={['image/*']} accessLevel="public" />
+      <Authenticator>
+        {({ signOut, user }) => (
+          <main>
+            <h1>Hello {user?.username}</h1>
+            <button onClick={signOut}>Sign out</button>
+          </main>
+        )}
+      </Authenticator>
+      <MapView />
+    </>
+  );
 }
-
-export default App;
