84514757dd89be690e8c4f24e590919a6ff7fea7
Caleb Pollman <cpollman@amazon.com>
2023-09-19 10:11:02 -0700

    chore(monorepo): clean up nohoist, update deps

--- examples/react-native/metro.config.js
+++ examples/react-native/metro.config.js
@@ -115,24 +115,24 @@ config.resolver.blockList = [
 ];
 
 // point to the example app deps for React related and deps requiring native modules
-config.resolver.extraNodeModules = {
-  // core React and React Native dependencies
-  react: path.resolve(__dirname, 'node_modules/react'),
-  'react-native': path.resolve(__dirname, 'node_modules/react-native'),
+// config.resolver.extraNodeModules = {
+//   // core React and React Native dependencies
+//   react: path.resolve(__dirname, 'node_modules/react'),
+//   'react-native': path.resolve(__dirname, 'node_modules/react-native'),
 
-  // xstate uses React under the hood
-  '@xstate/react': path.resolve(__dirname, 'node_modules/@xstate/react'),
+//   // xstate uses React under the hood
+//   '@xstate/react': path.resolve(__dirname, 'node_modules/@xstate/react'),
 
-  // depedendencies with native modules
-  '@react-native-picker/picker': path.resolve(
-    __dirname,
-    'node_modules/@react-native-picker/picker'
-  ),
-  'react-native-safe-area-context': path.resolve(
-    __dirname,
-    'node_modules/react-native-safe-area-context'
-  ),
-};
+//   // depedendencies with native modules
+//   '@react-native-picker/picker': path.resolve(
+//     __dirname,
+//     'node_modules/@react-native-picker/picker'
+//   ),
+//   'react-native-safe-area-context': path.resolve(
+//     __dirname,
+//     'node_modules/react-native-safe-area-context'
+//   ),
+// };
 
 // base transform values included by `react-native init`
 config.transformer.getTransformOptions = async () => ({
